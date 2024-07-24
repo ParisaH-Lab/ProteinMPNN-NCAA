733d9eecb8f5c1e0fc96a098950065401f17378d
laurjones <laurenjonesy2@gmail.com>
2024-07-24 12:40:44 -0700

    Update script versions.

--- training/custom_scripts/coordinate_mirroring_script.py
+++ training/custom_scripts/coordinate_mirroring_script.py
@@ -2,15 +2,14 @@
 
 import os
 import torch
-import numpy as np
 import shutil
 
-input_directory = '/projects/bgmp/lmjone/internship/ProteinMPNN-PH/training/datasets/pdb_2021aug02'
-output_directory = '/projects/bgmp/lmjone/internship/ProteinMPNN-PH/training/mirrored_pdb_2021aug02/pdb'
+input_directory = '/projects/bgmp/lmjone/internship/ProteinMPNN-PH/training/datasets/pdb_2021aug02_sample'
+output_directory = '/projects/bgmp/lmjone/internship/ProteinMPNN-PH/training/mirrored_pdb_2021aug02_sample/pdb'
 
 def mirror_xyz(xyz):
     mirrored_xyz = xyz.copy()
-    mirrored_xyz[:, :, 0] = -mirrored_xyz[:, :, 0]
+    mirrored_xyz[:, :, 0] = -mirrored_xyz[:, :, 0]  # Mirror the x-coordinates
     return mirrored_xyz
 
 def process_file(input_filepath, output_filepath):
@@ -18,21 +17,25 @@ def process_file(input_filepath, output_filepath):
     if 'xyz' in data:
         mirrored_xyz = mirror_xyz(data['xyz'].numpy())
         data['xyz'] = torch.tensor(mirrored_xyz)
-        torch.save(data, output_filepath)
-    else:
-        shutil.copyfile(input_filepath, output_filepath)
+    torch.save(data, output_filepath)
 
 def process_directory(input_dir, output_dir):
-    for item in os.listdir(input_dir):
-        input_item_path = os.path.join(input_dir, item)
-        output_item_path = os.path.join(output_dir, item)
-        if os.path.isdir(input_item_path):
-            os.makedirs(output_item_path, exist_ok=True)
-            process_directory(input_item_path, output_item_path)
-        elif item.endswith('.pt'):
-            process_file(input_item_path, output_item_path)
-        else:
-            shutil.copyfile(input_item_path, output_item_path)
+    for root, dirs, files in os.walk(input_dir):
+        for file in files:
+            if file.endswith('.pt'):
+                input_filepath = os.path.join(root, file)
+                relative_path = os.path.relpath(root, input_dir)
+                output_root = os.path.join(output_dir, relative_path)
+                os.makedirs(output_root, exist_ok=True)
+                output_filepath = os.path.join(output_root, file)
+                process_file(input_filepath, output_filepath)
+            else:
+                input_filepath = os.path.join(root, file)
+                relative_path = os.path.relpath(root, input_dir)
+                output_root = os.path.join(output_dir, relative_path)
+                os.makedirs(output_root, exist_ok=True)
+                output_filepath = os.path.join(output_root, file)
+                shutil.copyfile(input_filepath, output_filepath)
 
 process_directory(input_directory, output_directory)
 print("All files have been processed and mirrored coordinates saved.")
