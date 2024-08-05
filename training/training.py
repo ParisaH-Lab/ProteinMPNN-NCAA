dee260fbe9e74f9ddb0e22769fd00cfcdae7be2f
laurjones <laurenjonesy2@gmail.com>
2024-08-05 12:18:29 -0700

    **Removed counter I was trying to use for normalization **Reverted train_loss, train_accuracy, and true_false functions back to original versions from Andrew

--- training/training.py
+++ training/training.py
@@ -30,7 +30,6 @@ from new_combo_module import NewComboChiral
 ########
 
 def main(args):
-    scaler = torch.cuda.amp.GradScaler() # Scales the loss and unscales the gradients automatically
     device = torch.device("cuda:0" if (torch.cuda.is_available()) else "cpu") # Moves models and tensors to the appropriate device for training (code compatible with both)
 
     base_folder = time.strftime(args.path_for_outputs, time.localtime())
@@ -44,6 +43,7 @@ def main(args):
             os.makedirs(base_folder + subfolder)
 
     PATH = args.previous_checkpoint
+
     logfile = base_folder + 'log.txt'
     if not PATH:
         with open(logfile, 'w') as f:
@@ -70,6 +70,7 @@ def main(args):
         args.batch_size = 1000
 
     train, valid, test = build_training_clusters(params, args.debug)
+
     train_set = PDB_dataset(list(train.keys()), loader_pdb, train, params)
     train_loader = torch.utils.data.DataLoader(train_set, worker_init_fn=worker_init_fn, **LOAD_PARAM)
     valid_set = PDB_dataset(list(valid.keys()), loader_pdb, valid, params)
@@ -120,15 +121,12 @@ def main(args):
         for e in range(args.num_epochs):
             t0 = time.time()
             e = epoch + e
-
-            # Start of the training loop for one epoch
-            model.train() # Set model to training mode
-
-            # Initialize variables 
-            train_sum = 0. # For summing the total loss
-            train_acc = 0. # For counting the to
-            train_total = 0 # For counting the total number of predictions
-            batch_count = 0 # For couting the number of batches
+            model.train()
+            
+            # Intialize metrics for training
+            train_sum = 0.0
+            train_acc = 0
+            train_weights = 0 
 
             if e % args.reload_data_every_n_epochs == 0:
                 if reload_c != 0:
@@ -141,128 +139,81 @@ def main(args):
                     q.put_nowait(executor.submit(get_pdbs, train_loader, 1, args.max_protein_length, args.num_examples_per_epoch))
                     p.put_nowait(executor.submit(get_pdbs, valid_loader, 1, args.max_protein_length, args.num_examples_per_epoch))
                 reload_c += 1
-
-            # Iterate over the training data
-            for i, batch in enumerate(loader_train):
+            for  batch in loader_train:
+                start_batch = time.time()
                 X, S, mask, lengths, chain_M, residue_idx, mask_self, chain_encoding_all = featurize(batch, device)
+                elapsed_featurize = time.time() - start_batch
                 optimizer.zero_grad()
-                
-                # Forward Pass through the model
+
                 output = model(X, S, mask, chain_M, residue_idx, chain_encoding_all) # Passes the input data through the model to obtain logits
                 
-                # Prepare the target tensor with the same shape as the output
-                # targets = torch.zeros(output.size(0), 2, device=output.device)
-                # targets[:,0] = 1.0
-                targets = torch.zeros(output.size(0), output.size(1), output.size(2), device=output.device)
-                targets[:, :, 0] = 1.0  
-
-                # Compute the loss
+                targets = torch.zeros(output.size(0), 2, device=output.device)
+                targets[:,0] = 1.
+                    
                 loss = criterion(output, targets) # Calculates the binary cross-entropy loss between model output and targets 
-                loss.backward() # Computes the gradients of the loss (backpropogation)
-                optimizer.step() # Updates the model parameters based on the gradients (optimizer update)
+                loss.backward() # Computes the gradients of the loss
+                optimizer.step() # Updates the model parameters based on the gradients
 
-                # Accumulate loss and accuracy
-                train_sum += loss.item()
-                predicted_classes = torch.argmax(output, -1)
-                true_classes = torch.argmax(targets, -1)
-                true_false = (predicted_classes == true_classes).float()
+                true_false = (torch.ones(output.size(0)) == torch.argmax(output,-1)).float()
+                train_sum += torch.sum(loss).cpu().data.numpy()
                 train_acc += torch.sum(true_false).cpu().data.numpy()
-                train_total += output.size(0)
-                total_step += 1
-                batch_count += 1
 
-            # Average loss and accuracy for the epoch
-            train_loss = train_sum / train_total
-            train_accuracy = train_acc / train_total
+                total_step += 1
 
             model.eval()
             with torch.no_grad():
-                validation_sum = 0.0
-                validation_acc = 0.0
-                validation_total = 0.0
+                validation_sum, validation_weights = 0., 0.
+                validation_acc, validation_total = 0., 0.
 
                 for i, batch in enumerate(loader_valid):
                     X, S, mask, lengths, chain_M, residue_idx, mask_self, chain_encoding_all = featurize(batch, device)
-                    output = model(X, S, mask, chain_M, residue_idx, chain_encoding_all)
-                    
-                    # Prepare the target tensor with the same shape as the output
-                    # targets = torch.zeros(output.size(0), 2, device=output.device)
-                    # targets[:,0] = 1.0
-                    targets = torch.zeros(output.size(0), output.size(1), output.size(2), device=output.device)
-                    targets[:, :, 0] = 1.0  # Assuming the first class is the correct one for simplicity
-                    
-                    loss = criterion(output, targets)
-                    validation_sum += loss.item() * output.size(0)
-                    true_false = (torch.argmax(output, -1) == torch.argmax(targets, -1)).float()
+                    log_probs = model(X, S, mask, chain_M, residue_idx, chain_encoding_all)
+
+                    targets = torch.zeros(log_probs.size(0), 2, device=log_probs.device)
+                    loss = criterion(log_probs, targets)
+                    true_false = (torch.ones(log_probs.size(0)) == torch.argmax(log_probs,-1)).float()
+
+                    validation_sum += torch.sum(loss).cpu().data.numpy()
                     validation_acc += torch.sum(true_false).cpu().data.numpy()
-                    validation_total += true_false.numel()
-
-                # Compute average validation loss and accuracy
-                validation_loss = validation_sum / validation_total  # Average loss per sample
-                validation_accuracy = validation_acc / validation_total  # Overall accuracy
-
-            # Record and print epoch metrics
-            t1 = time.time()
-            dt = t1 - t0
-            formatted_output = (
-                f'epoch: {e+1}, step: {total_step}, time: {dt:.1f}, '
-                f'train: {train_loss:.3f}, valid: {validation_loss:.3f}, '
-                f'train_acc: {train_accuracy:.3f}, valid_acc: {validation_accuracy:.3f}'
-            )
-            with open(logfile, 'a') as f:
-                f.write(formatted_output + '\n')
-            print(formatted_output)
-
-            # Save model checkpoints
-            checkpoint_filename_last = base_folder + 'model_weights/epoch_last.pt'
-            torch.save({
-                'epoch': e + 1,
-                'step': total_step,
-                'model_state_dict': model.state_dict(),
-                'optimizer_state_dict': optimizer.optimizer.state_dict(),
-            }, checkpoint_filename_last)
-
-            if (e + 1) % args.save_model_every_n_epochs == 0:
-                checkpoint_filename = base_folder + f'model_weights/epoch{e+1}_step{total_step}.pt'
-                torch.save({
-                    'epoch': e + 1,
-                    'step': total_step,
-                    'model_state_dict': model.state_dict(),
-                    'optimizer_state_dict': optimizer.optimizer.state_dict(),
-                }, checkpoint_filename)
-
-            #      # Record and print epoch metrics
-            #     t1 = time.time()
-            #     dt = t1 - t0
-            #     formatted_output = (
-            #         f'epoch: {e+1}, step: {total_step}, time: {dt:.1f}, '
-            #         f'train: {train_loss:.3f}, valid: {validation_loss:.3f}, '
-            #         f'train_acc: {train_accuracy:.3f}, valid_acc: {validation_accuracy:.3f}'
-            #     )
-            #     with open(logfile, 'a') as f:
-            #         f.write(formatted_output + '\n')
-            #     print(formatted_output)
             
-            # checkpoint_filename_last = base_folder+'model_weights/epoch_last.pt'.format(e+1, total_step)
-            # torch.save({
-            #             'epoch': e+1,
-            #             'step': total_step,
-            #             'num_edges' : args.num_neighbors,
-            #             'noise_level': args.backbone_noise,
-            #             'model_state_dict': model.state_dict(),
-            #             'optimizer_state_dict': optimizer.optimizer.state_dict(),
-            #             }, checkpoint_filename_last)
+            train_loss = train_sum 
+            train_accuracy = train_acc 
+            train_perplexity = np.exp(train_loss)
+            validation_loss = validation_sum 
+            validation_accuracy = validation_acc 
+            validation_perplexity = np.exp(validation_loss)
+            
+            train_perplexity_ = np.format_float_positional(np.float32(train_perplexity), unique=False, precision=3)     
+            validation_perplexity_ = np.format_float_positional(np.float32(validation_perplexity), unique=False, precision=3)
+            train_accuracy_ = np.format_float_positional(np.float32(train_accuracy), unique=False, precision=3)
+            validation_accuracy_ = np.format_float_positional(np.float32(validation_accuracy), unique=False, precision=3)
+    
+            t1 = time.time()
+            dt = np.format_float_positional(np.float32(t1-t0), unique=False, precision=1) 
+            with open(logfile, 'a') as f:
+                f.write(f'epoch: {e+1}, step: {total_step}, time: {dt}, train: {train_perplexity_}, valid: {validation_perplexity_}, train_acc: {train_accuracy_}, valid_acc: {validation_accuracy_}\n')
+            print(f'epoch: {e+1}, step: {total_step}, time: {dt}, train: {train_perplexity_}, valid: {validation_perplexity_}, train_acc: {train_accuracy_}, valid_acc: {validation_accuracy_}')
+            
+            checkpoint_filename_last = base_folder+'model_weights/epoch_last.pt'.format(e+1, total_step)
+            torch.save({
+                        'epoch': e+1,
+                        'step': total_step,
+                        'num_edges' : args.num_neighbors,
+                        'noise_level': args.backbone_noise,
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.optimizer.state_dict(),
+                        }, checkpoint_filename_last)
 
-            # if (e+1) % args.save_model_every_n_epochs == 0:
-            #     checkpoint_filename = base_folder+'model_weights/epoch{}_step{}.pt'.format(e+1, total_step)
-            #     torch.save({
-            #             'epoch': e+1,
-            #             'step': total_step,
-            #             'num_edges' : args.num_neighbors,
-            #             'noise_level': args.backbone_noise, 
-            #             'model_state_dict': model.state_dict(),
-            #             'optimizer_state_dict': optimizer.optimizer.state_dict(),
-            #             }, checkpoint_filename)
+            if (e+1) % args.save_model_every_n_epochs == 0:
+                checkpoint_filename = base_folder+'model_weights/epoch{}_step{}.pt'.format(e+1, total_step)
+                torch.save({
+                        'epoch': e+1,
+                        'step': total_step,
+                        'num_edges' : args.num_neighbors,
+                        'noise_level': args.backbone_noise, 
+                        'model_state_dict': model.state_dict(),
+                        'optimizer_state_dict': optimizer.optimizer.state_dict(),
+                        }, checkpoint_filename)
 
 ############
 # ARGPARSE #
